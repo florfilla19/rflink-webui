b13bb5ed55700b3e626e5d3dac510b32153e8f06
florfilla19 <39666815+florfilla19@users.noreply.github.com>
2026-09-26 18:19:50 +0200

    ui: align Wifi.vue with v2

--- src/pages/Wifi.vue
+++ src/pages/Wifi.vue
@@ -1,51 +1,71 @@
 <template>
-	<div>
-		<h1 class="page-title" style="text-align: center;">Wifi</h1>
-		<div class="container">
-			<h2 style="margin: 5px 0;">Client</h2>
-			<table>
-				<AutoTypeField v-for="field in fields_client" :key="field.key" :configuration="field" v-on:input="updateValue_wifi(field, $event)"></AutoTypeField>
-			</table>
-			<input type="submit" class="btn-green" @click="save_config" value="Save">
-		</div>
-		<div class="container">
-			<h2 style="margin: 5px 0;">AP</h2>
-			<table>
-				<AutoTypeField v-for="field in fields_ap" :key="field.key" :configuration="field" v-on:input="updateValue_wifi(field, $event)"></AutoTypeField>
-			</table>
-			<input type="submit" class="btn-green" @click="save_config" value="Save">
-		</div>
-	</div>
+  <div>
+    <h1 class="page-title">Configuration réseau</h1>
+    <p class="page-subtitle">Paramètres du Wi-Fi client et du point d'accès</p>
+
+    <div class="container">
+      <h2>Wi-Fi client</h2>
+      <table>
+        <AutoTypeField v-for="field in fields_client" :key="field.key" :configuration="field" @input="updateValue_wifi(field, $event)" />
+      </table>
+      <div class="form-actions">
+        <button class="btn-green" @click="save_config">▣ &nbsp; Enregistrer</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <h2>Point d'accès</h2>
+      <table>
+        <AutoTypeField v-for="field in fields_ap" :key="field.key" :configuration="field" @input="updateValue_wifi(field, $event)" />
+      </table>
+      <div class="form-actions">
+        <button class="btn-green" @click="save_config">▣ &nbsp; Enregistrer</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	import {api_mixin} from "../api_mixin";
-	import AutoTypeField from "../components/AutoTypeField";
-	import {generateKeysMapper, sortFunction} from "../definitons";
+import { api_mixin } from "../api_mixin";
+import AutoTypeField from "../components/AutoTypeField";
+import { generateKeysMapper, sortFunction } from "../definitons";
 
-	export default {
-		name: "Wifi",
-		mixins: [api_mixin],
-		components: {AutoTypeField},
-		computed: {
-			fields_client() {
-				if(this.config.wifi ==null) return []
-				return Object.keys(this.config.wifi).map(generateKeysMapper(this.config, "wifi", "client" )).filter((x)=>{ return !!x }).sort(sortFunction)
-			},
-
-			fields_ap() {
-				if(this.config.wifi ==null) return []
-				return Object.keys(this.config.wifi).map(generateKeysMapper(this.config, "wifi", "ap")).filter((x)=>{ return !!x }).sort(sortFunction)
-			}
-		},
-		methods: {
-			updateValue_wifi(field,value) {
-				this.config.wifi[field.key] = value
-			}
-		},
-	}
+export default {
+  name: "Wifi",
+  mixins: [api_mixin],
+  components: { AutoTypeField },
+  computed: {
+    fields_client() {
+      if (this.config.wifi == null) return [];
+      return Object.keys(this.config.wifi).map(generateKeysMapper(this.config, "wifi", "client")).filter(x => !!x).sort(sortFunction);
+    },
+    fields_ap() {
+      if (this.config.wifi == null) return [];
+      return Object.keys(this.config.wifi).map(generateKeysMapper(this.config, "wifi", "ap")).filter(x => !!x).sort(sortFunction);
+    }
+  },
+  methods: {
+    updateValue_wifi(field, value) {
+      this.config.wifi[field.key] = value;
+    }
+  }
+};
 </script>
 
 <style scoped>
+.form-actions {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 14px;
+}
 
+.form-actions button {
+  min-width: 150px;
+}
+
+@media (max-width: 560px) {
+  .form-actions button {
+    width: 100%;
+  }
+}
 </style>
